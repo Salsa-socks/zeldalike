94b5ea0350b1d25f25fde218332521c4a0d29bc2
Bongani NKOSI <bnkosi@c5r4s5.wethinkcode.co.za>
2019-10-01 15:13:52 +0200

    added font and room canvas, working on script - room text bool in Roommove script

--- YTzeldalike/Assets/Scripts/RoomMove.cs
+++ YTzeldalike/Assets/Scripts/RoomMove.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomMove : MonoBehaviour
 {
     public Vector2 cameraChange;
     public Vector3 playerChange;
     private CamMovement cam;
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +32,18 @@ public class RoomMove : MonoBehaviour
             cam.minPos += cameraChange;
             cam.maxPos += cameraChange;
             other.transform.position += playerChange;
+            if (needText)
+            {
+                StartCoroutine(placeNameCo());
+            }
         }
+    }
+
+    private IEnumerator placeNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
     }
 }
